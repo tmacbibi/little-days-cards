2fd6357871897ac6c8fa2b7b9a9f5163e2f65e41
tmacbibi <tmacbibi@gmail.com>
2026-09-19 16:14:21 +0800

    Load v0.9 feedback refinements

--- app.js
+++ app.js
@@ -1,1 +1,1 @@
-(()=>{const css=document.createElement('link');css.rel='stylesheet';css.href='v08.css?v=0.8.0';document.head.appendChild(css);const s=document.createElement('script');s.src='v08.js?v=0.8.0';s.defer=true;document.head.appendChild(s)})();
+(()=>{const css=document.createElement('link');css.rel='stylesheet';css.href='v09.css?v=0.9.0';document.head.appendChild(css);const s=document.createElement('script');s.src='v09.js?v=0.9.0';s.defer=true;document.head.appendChild(s)})();
