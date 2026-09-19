76e3b66bc014cd6b8cd8b1d672559fa4a15ed91a
tmacbibi <tmacbibi@gmail.com>
2026-09-19 13:41:05 +0800

    Refresh offline cache for v0.4

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='little-days-cards-v0.3.0';
-const ASSETS=['./','./index.html','./styles.css','./v03.css','./app.js','./v03.js','./manifest.webmanifest','./icons/icon-180.png','./icons/icon-192.png','./icons/icon-512.png'];
+const CACHE='little-days-cards-v0.4.0';
+const ASSETS=['./','./index.html','./styles.css','./v04.css','./app.js','./v04.js','./manifest.webmanifest','./icons/icon-180.png','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;const u=new URL(e.request.url);if(u.origin!==location.origin)return;e.respondWith(fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res}).catch(()=>caches.match(e.request).then(x=>x||caches.match('./index.html'))))});
