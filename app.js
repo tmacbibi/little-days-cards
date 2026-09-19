bdd7ecfe99108f78477e9f7d6e8a19e0275a6d7c
tmacbibi <tmacbibi@gmail.com>
2026-09-19 15:34:51 +0800

    Load v0.7 simplified people manager

--- app.js
+++ app.js
@@ -1,1 +1,1 @@
-(()=>{const css=document.createElement('link');css.rel='stylesheet';css.href='v06.css?v=0.6.0';document.head.appendChild(css);const s=document.createElement('script');s.src='v06.js?v=0.6.0';s.defer=true;document.head.appendChild(s)})();
+(()=>{const css=document.createElement('link');css.rel='stylesheet';css.href='v07.css?v=0.7.0';document.head.appendChild(css);const s=document.createElement('script');s.src='v07.js?v=0.7.0';s.defer=true;document.head.appendChild(s)})();
